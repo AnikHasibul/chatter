0b03bee651172be276329f44a5da395f2670308c
AnikHasibul <anikhasibul@outlook.com>
2019-01-22 13:49:15 +0600

    serving index.html not from file

--- api/handlers/chatWindow.go
+++ api/handlers/chatWindow.go
@@ -3,18 +3,28 @@ package handlers
 import (
 	"io"
 	"net/http"
-	"os"
+	"strings"
 )
 
 // ChatFrontEnd serves the frontend/html/view page for the chat services.
 func ChatFrontEnd(w http.ResponseWriter, r *http.Request) {
 	// open the index page
-	f, err := os.Open("index.html")
-	if err != nil {
-		log.Error(err)
-	}
+	index := strings.NewReader(`
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="initial-scale=1">
+	<title>Chatter • </title>
+	<script src="/app/script.js" async defer></script>
+</head>
+<body>
+
+</body>
+</html>
+	`)
 	// serve the page
-	_, err = io.Copy(w, f)
+	_, err := io.Copy(w, index)
 	if err != nil {
 		log.Error(err)
 	}
